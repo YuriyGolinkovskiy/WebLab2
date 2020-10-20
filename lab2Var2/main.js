48d70699a0bc6261eca81040c4b05cc1f2d2fd98
YuriyGolinkovskiy <45183992+YuriyGolinkovskiy@users.noreply.github.com>
2020-10-20 18:18:01 +0300

    init

---
+++ lab2Var2/main.js
@@ -0,0 +1,13 @@
+$('.ui.dropdown')
+  .dropdown({on: 'hover'})
+;
+$('.ui.selection')
+  .dropdown({on: 'click'})
+;
+$('.special.cards .image').dimmer({
+  on: 'hover'
+});
+
+$('.ui.accordion')
+  .accordion()
+;
